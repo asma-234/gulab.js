b87279e0a2a064e4c71ef88bdfdb449054012364
Gulab <asma.gulab_ti24@nusaputra.ac.id>
2025-09-28 18:29:03 +0700

    Assignment

--- week 2/class work/main.js
+++ week 2/class work/main.js
@@ -1,9 +1,63 @@
-alert('Welcome to the app');
+//  Swap the values
+console.log('Swaping values');
+let x= 10;
+let y= 20;
+let temp= x;
+x=y;
+y= temp;
+console.log('x=',x);
+console.log('y=',y);
 
-let $name = prompt('Enter your name');
-alert('Your name is ' + $name);
+// Simple Math with Variables
+console.log('Simple Math');
 
-let isStudent = confirm('Are you student?');
-alert('You are student ' + isStudent);
+let a= 8;
+let b= 3;
+let sum= a+b;
+let product= a*b;
+let average= (a+b)/2;
 
-document.getElementById('name').innerText = 'Your name is: ' + $name;
+console.log('sum=', sum);
+console.log('product=', product);
+console.log('average=', average);
+
+// Temperature Conversion
+console.log('Temperature Conversion');
+
+let C= 25;
+let R= (4/5)* C;
+let F= (9/5)* C + 32;
+let K= C + 273.15;
+
+console.log('Celsius:', C);
+console.log('Reaumur:', R);
+console.log('Fahrenheit:', F);
+console.log('Kelvin:', K);
+
+// Predict the value 
+console.log('Predicting value')
+
+let e= 5;
+let f= 2;
+
+let result= e + f * e; 
+console.log(result);  
+
+// Combine Strings
+console.log('Combining Strings')
+
+let firstName= "John";
+let lastName= "Doe"; 
+let fullName= firstName + " " + lastName;
+
+console.log(fullName); 
+
+// Guess the Output
+console.log('Guessing the Output')
+
+let p= 10; 
+let q= x;
+p = 20;
+
+console.log(q);
+
