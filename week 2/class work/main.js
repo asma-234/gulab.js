0ca49c7878a7a20d76cd01c4b518216fbd6a6d30
Gulab <asma.gulab_ti24@nusaputra.ac.id>
2025-09-28 18:35:59 +0700

    Delete week 2/class work directory

--- week 2/class work/main.js
+++
@@ -1,9 +0,0 @@
-alert('Welcome to the app');
-
-let $name = prompt('Enter your name');
-alert('Your name is ' + $name);
-
-let isStudent = confirm('Are you student?');
-alert('You are student ' + isStudent);
-
-document.getElementById('name').innerText = 'Your name is: ' + $name;
