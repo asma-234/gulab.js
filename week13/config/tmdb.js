139e8a43a21ad28edf1b1a3ecd6e050e7363af20
Gulab <asma.gulab_ti24@nusaputra.ac.id>
2025-12-29 00:39:21 +0700

    Delete week13 directory

--- week13/config/tmdb.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios");
-
-const tmdbClient = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
-  timeout: 5000,
-});
-
-module.exports = tmdbClient;
--- week13/routes/movieRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { searchMovies } = require("../controllers/movieController");
-const { validateSearchQuery } = require("../middleware/validateRequest");
-
-router.get("/search", validateSearchQuery, searchMovies);
-
-module.exports = router;
