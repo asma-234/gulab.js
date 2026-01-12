dd996f0f1d214d79a93182f73b0cff4aabeb9f6e
Gulab <asma.gulab_ti24@nusaputra.ac.id>
2026-01-12 23:04:04 +0700

    Delete Routes/api directory

--- Routes/api/auth.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const AuthController = require("../../controllers/AuthController");
-
-router.post("/login", AuthController.login);
-
-module.exports = router;
--- Routes/api/vacancies.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const VacancyController = require("../../controllers/VacancyController");
-
-router.get("/", VacancyController.list);
-
-module.exports = router;
